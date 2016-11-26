9dc158a002efd9e7ec0fd9444fb8de122352632d
Diego Stratta <strattadb@gmail.com>
2016-11-25 22:10:42 -0300

    Fix bug with custom cd

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Get current dir (so run this script from anywhere)
 
 export DOTFILES_DIR
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOTFILES_DIR="$( builtin cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update dotfiles itself first
 
