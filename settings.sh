bb6ba8262637a68c819b7bf1aa70fd23dd993740
Diego Stratta <strattadb@gmail.com>
2016-11-26 00:00:25 -0300

    Add settings.sh

---
+++ settings.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# Disable mouse acceleration
+sudo sh -c "echo 'Section \"InputClass\"' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
+sudo sh -c "echo '    Identifier \"mouse\"' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
+sudo sh -c "echo '    MatchIsPointer \"on\"' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
+sudo sh -c "echo '    Option \"AccelerationProfile\" \"-1\"' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
+sudo sh -c "echo '    Option \"AccelerationScheme\" \"none\"' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
+sudo sh -c "echo 'EndSection' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
