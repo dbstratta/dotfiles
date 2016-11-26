1cd012fbd78ef0ddc53947d7abc784518737037f
Diego Stratta <strattadb@gmail.com>
2016-11-26 11:45:45 -0300

    Set GTK and icon themes

--- settings.sh
+++ settings.sh
@@ -7,3 +7,8 @@ sudo sh -c "echo '    MatchIsPointer \"on\"' >> /usr/share/X11/xorg.conf.d/90-mo
 sudo sh -c "echo '    Option \"AccelerationProfile\" \"-1\"' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
 sudo sh -c "echo '    Option \"AccelerationScheme\" \"none\"' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
 sudo sh -c "echo 'EndSection' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
+
+# Set GTK and Icon theme
+gsettings set org.gnome.desktop.interface gtk-theme 'Adapta'
+gsettings set org.gnome.desktop.wm.preferences theme 'Adapta'
+gsettings set org.gnome.desktop.interface icon-theme 'Paper'
