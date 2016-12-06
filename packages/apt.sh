c8b57e8306f7f28b47a8d83deb220c16f3ffa1c3
Diego Stratta <strattadb@gmail.com>
2016-12-06 00:00:48 -0300

    Remove Neovim package and repository

--- packages/apt.sh
+++ packages/apt.sh
@@ -10,8 +10,6 @@ sudo apt-add-repository -y ppa:tista/adapta
 sudo apt-add-repository -y ppa:snwh/pulp
 # Atom editor
 sudo add-apt-repository -y ppa:webupd8team/atom
-# Neovim
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
 # Google Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
@@ -29,7 +27,6 @@ apps=(
   python3-dev
   python3-pip
   atom
-  neovim
   google-chrome-stable
   firefox
   unity-tweak-tool
