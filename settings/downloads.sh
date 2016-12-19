3af0a3bd972aa3dcb579992b4dbb20abea73803b
Diego Stratta <strattadb@gmail.com>
2016-12-18 21:11:23 -0300

    Download Vim Plug for Neovim and install Neovim packages

--- settings/downloads.sh
+++ settings/downloads.sh
@@ -1,12 +1,18 @@
-# Downloads Vim Plug Bundle manager for Vim
+# Downloads Vim Plug bundle manager for Vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Remove unused Vim plugins (to remain idempotent)
+# Download Vim Plug bundle manager for Neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Remove unused Vim and Neovim plugins (to remain idempotent)
 vim -E -c PlugClean! -c qall
+nvim -E -c PlugClean! -c qall
 
 # Clone bundle repositories with Vim Plug and install them
 vim -E -c PlugInstall -c qall
+nvim -E -c PlugInstall -c qall
 
 # Download Tmux Plugin Manager
 rm -rf ~/.tmux/plugins/tpm
