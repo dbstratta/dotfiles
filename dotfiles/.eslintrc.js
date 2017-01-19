49d5440ba34f71aa50e3069adabc38e2513b1ba7
Diego Stratta <strattadb@gmail.com>
2017-01-18 22:19:24 -0300

    Update .eslintrc.js

--- dotfiles/.eslintrc.js
+++ dotfiles/.eslintrc.js
@@ -2,21 +2,26 @@ module.exports = {
   "root": true,
   "env": {
     "browser": true,
-    "commonjs": true,
+    "node": true,
     "es6": true,
+    "commonjs": true,
   },
   "extends": "eslint:recommended",
+  "parser": "babel-eslint",
   "parserOptions": {
     "sourceType": "module",
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-    },
+    "allowImportExportEverywhere": true,
   },
+  "plugins": [
+    "html",
+  ],
   "rules": {
     "indent": ["error", 2, { "SwitchCase": 1 }],
     "linebreak-style": ["error", "unix"],
     "quotes": ["error", "single"],
     "semi": ["error", "always"],
+    "no-extra-semi": "error",
+    "no-console": 0,
     "comma-dangle": ["error", "always-multiline"],
     "eol-last": ["error", "always"],
     "max-len": ["error", {
