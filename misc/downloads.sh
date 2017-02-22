92382768942d980a5b4aa42ae5a82511684340a1
Diego Stratta <strattadb@gmail.com>
2017-02-21 21:55:03 -0300

    Update downloads.sh

--- misc/downloads.sh
+++ misc/downloads.sh
@@ -1,9 +1,9 @@
-# Downloads Vim Plug bundle manager for Vim
+# Downloads Vim Plug bundle manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Remove unused Vim and Neovim plugins (to remain idempotent)
-vim -E -c PlugClean! -c qall
+# Remove unused Neovim and Neovim plugins (to remain idempotent)
+nvim -E -c PlugClean! -c qall
 
 # Clone bundle repositories with Vim Plug and install them
-vim -E -c PlugInstall -c qall
+nvim -E -c PlugInstall -c qall
