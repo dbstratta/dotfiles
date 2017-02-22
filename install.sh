42ffe7a4a3e4ce8b7f01ad36eb3229dce1b13a0c
Diego Stratta <strattadb@gmail.com>
2017-02-21 21:45:44 -0300

    Improve install.sh

--- install.sh
+++ install.sh
@@ -1,51 +1,40 @@
 #!/usr/bin/env bash
 
 # Get current dir (so run this script from anywhere)
-export REPO_DIR
-REPO_DIR="$( builtin cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export REPO_DIR="$( builtin cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update dotfiles itself first
-[ -d "$REPO_DIR/.git" ] && git --work-tree="$REPO_DIR" \
---git-dir="$REPO_DIR/.git" pull origin master
+[ -d "${REPO_DIR}/.git" ] && git --work-tree="${REPO_DIR}" \
+--git-dir="${REPO_DIR}/.git" pull origin master
 
 # Install all packages
-. "$REPO_DIR/packages/install_packages.sh"
+. "${REPO_DIR}/packages/install_packages.sh"
 
 # Settings and fonts
-. "$REPO_DIR/misc/downloads.sh"
-. "$REPO_DIR/misc/settings.sh"
+. "${REPO_DIR}/misc/downloads.sh"
+. "${REPO_DIR}/misc/settings.sh"
 
-# Create needed folders
+# Create some folders
 mkdir -p ~/workspace
 mkdir -p ~/projects
 mkdir -p ~/wallpapers
 
 # Dotfiles symlinks
-DOTFILES=$REPO_DIR/dotfiles/.*
-for dotfile in $DOTFILES; do
-  if [[ -f $dotfile ]]; then
-    ln -sfv "$dotfile" ~
-  fi
+for dotfile in "${REPO_DIR}/dotfiles/.*"; do
+  [ -f "${dotfile}" ] && ln -sfv "${dotfile}" ~/
 done
 
 # Fonts symlink
-FONTS_DIR=$REPO_DIR/dotfiles/.fonts
 rm -rf ~/.fonts
-ln -sfv "$FONTS_DIR" ~/
+ln -sfvn "${REPO_DIR}/fonts" ~/.fonts
 
 # Scripts symlink
-SCRIPTS_DIR=$REPO_DIR/dotfiles/.scripts/
 rm -rf ~/.scripts
-ln -sfv "$SCRIPTS_DIR" ~/
+ln -sfvn "${REPO_DIR}/scripts" ~/.scripts
 
-CONFIG_FOLDERS_DIR=$REPO_DIR/dotfiles/.config/*
-for file in $CONFIG_FOLDERS_DIR; do
-  ln -sfv "$file" ~/.config
-done
+# Config files symlinks
+ln -sfv "${REPO_DIR}/dotfiles/.config/*" ~/.config/
 
 # Atom editor config files symlinks
-ATOM_CONFIG_FILES=$REPO_DIR/dotfiles/.atom/*
 mkdir -p ~/.atom
-for file in $ATOM_CONFIG_FILES; do
-  ln -sfv "$file" ~/.atom
-done
+ln -sfv "${REPO_DIR}/dotfiles/.atom/*" ~/.atom/
