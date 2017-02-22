d7212c87ca1b5ea034307a5d60060501eb075e5d
Diego Stratta <strattadb@gmail.com>
2017-02-21 21:44:47 -0300

    Improve install_pacaur.sh script

--- scripts/install_pacaur.sh
+++ scripts/install_pacaur.sh
@@ -2,7 +2,7 @@
 
 # Create a temporary directory and cd into it
 build_root="$(mktemp -d)"
-cd "${build_root}"
+cd "$build_root"
 
 # Clone Cower repo
 git clone https://aur.archlinux.org/cower.git
@@ -14,12 +14,12 @@ gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
 # Build the package
 makepkg -sric --noconfirm
 
-cd "${build_root}"
+cd "$build_root"
 
 # Now it's time to build Pacaur
 git clone https://aur.archlinux.org/pacaur.git
-cd ./pacaur
+cd pacaur
 makepkg -sric --noconfirm
 
-cd
-rm -rf "${build_root}"
+cd ~
+rm -rf "$build_root"
