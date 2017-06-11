60e34be3255a1ec1577b4305f85957722a5c92f1
Diego Stratta <strattadb@gmail.com>
2017-06-11 06:05:19 -0300

    Change append operator to output operator

--- misc/settings.sh
+++ misc/settings.sh
@@ -1,5 +1,5 @@
 # Disable mouse acceleration
-sudo sh -c "echo 'Section \"InputClass\"' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
+sudo sh -c "echo 'Section \"InputClass\"' > /usr/share/X11/xorg.conf.d/90-mouse.conf"
 sudo sh -c "echo '    Identifier \"mouse\"' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
 sudo sh -c "echo '    MatchIsPointer \"on\"' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
 sudo sh -c "echo '    Option \"AccelerationProfile\" \"-1\"' >> /usr/share/X11/xorg.conf.d/90-mouse.conf"
